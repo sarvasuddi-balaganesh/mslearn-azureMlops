509016cec1e1b426aef73eacdd30ea3122433ea3
sarvasudi-balaganesh <sarvasudi.balaganesh@nokia.com>
2023-05-30 06:05:52 +0000

    Modifies job.yaml file

--- src/model/train.py
+++ src/model/train.py
@@ -7,7 +7,7 @@ import mlflow
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.model_selection import train_test_split
 
 # define functions
 def main(args):
@@ -35,7 +35,7 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
     return train_test_split(X, y, test_size=0.30, random_state=0)
 
 
